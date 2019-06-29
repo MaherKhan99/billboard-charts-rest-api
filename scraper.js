7909fbabb42ab1c3055994c414d47233ba3f7014
Maher Khan <khanm167@mcmaster.ca>
2019-06-29 18:25:04 -0400

    parse scraped html

--- scraper.js
+++ scraper.js
@@ -1,9 +1,24 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 const url = "https://www.billboard.com/charts/hot-100";
 
-axios(url)
+axios.get(url)
     .then(response => {
-        console.log(response.data);
+        const chartArray = []
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const chart = $('.chart-list-item')
+        const values = Object.values(chart)
+        values.forEach(song => {
+            if(song['attribs']) {
+                chartArray.push({
+                    title: song['attribs']['data-title'],
+                    artist: song['attribs']['data-artist'],
+                    rank: song['attribs']['data-rank']
+                })
+            }
+        })
+        console.log(chartArray)
     })
     .catch(console.error);
