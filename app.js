1d9089b3baea663c7d35cfab65e6876a8894eab8
Maher Khan <khanm167@mcmaster.ca>
2019-06-30 15:01:23 -0400

    added GET route for all songs

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const Song = require('./models/song');
+
+mongoose.connect('mongodb://billboardapi:billboardapi1@ds345587.mlab.com:45587/billboard-chart-rest-api', { useNewUrlParser: true })
 
 app.get('/', function(req, res) {
     res.send('hello');
+})
+
+app.get('/songs', function (req, res) {
+    const allSongs = Song.find({}).sort({rank: 1})
+    Song.find(allSongs, function(err, songList){
+        if(err) {
+            console.log(err)
+        } else {
+            res.send(songList)
+        }
+    })
 })
 
 app.listen(3000, function() {
