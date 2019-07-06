3918957a38b16ad8a0048ed3362e8f0b7b45247f
Maher Khan <khanm167@mcmaster.ca>
2019-07-05 21:55:56 -0400

    implemented cron job for web scraper

--- scraper.js
+++ scraper.js
@@ -1,28 +1,33 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose')
+const CronJob = require('cron').CronJob;
 const Song = require('./models/song')
 require('dotenv').config();
 
 const url = "https://www.billboard.com/charts/hot-100";
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds345587.mlab.com:45587/billboard-chart-rest-api`, { useNewUrlParser: true })
 
-axios.get(url)
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const chart = $('.chart-list-item')
-        const values = Object.values(chart)
-        Song.deleteMany({}, () => console.log('deleted songs'))
-        values.forEach(song => {
-            if(song['attribs']) {
-                Song.create({
-                    title: song['attribs']['data-title'],
-                    artist: song['attribs']['data-artist'],
-                    rank: song['attribs']['data-rank']
-                })
-            }
+const job = new CronJob('1 0 * * *', function () {
+    axios.get(url)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const chart = $('.chart-list-item')
+            const values = Object.values(chart)
+            Song.deleteMany({}, () => console.log('deleted songs'))
+            values.forEach(song => {
+                if(song['attribs']) {
+                    Song.create({
+                        title: song['attribs']['data-title'],
+                        artist: song['attribs']['data-artist'],
+                        rank: song['attribs']['data-rank']
+                    })
+                }
+            })
+            console.log('Successful scrape!')
         })
-        console.log('Successful scrape!')
-    })
-    .catch(console.error);
+        .catch(console.error);
+})
+
+job.start();
